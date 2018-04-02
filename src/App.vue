28d5d9c14f88ad65644be1d2b22a723286d69e5b
chenfangyuan <15929720704@163.com>
2018-04-02 20:33:10 +0800

    hello

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FootGuide/>
+  </div>
+</template>
+
+<script>
+import FootGuide from './components/FootGuide/FootGuide.vue'
+
+export default {
+  components: {
+   FootGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
